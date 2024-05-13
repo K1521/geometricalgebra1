3fa06b371075becc524b95714d9bf114e5acd13c
konst_dell <konstantin.bachem@stud.tu-darmstadt.de>
2024-05-14 01:50:06 +0200

    perpendicularcubev2

--- linedistderiv/perpendicular4.py
+++ linedistderiv/perpendicular4.py
@@ -77,36 +77,56 @@ pdata.lines = np.array([[2,0,1]])
 squareidx=([0,0,1,1],[0,1,0,1],0)#xy
 festeaxis=2
 
+def evalsquare(festeaxis,offset):
+    squareidx=[0,np.array([0,0,1,1]),np.array([0,1,0,1])]#index for square
+    squareidx=squareidx[-festeaxis:]+squareidx[:-festeaxis]#index for square alligned with axis
+    #print(squareidx)
+    for i in range(3):#move the square
+        squareidx[i]+=offset[i]
+    #print(offset_indices)
+    squarep=points[tuple(squareidx)]
+    #squarev = np.apply_along_axis(gaussnewton, -1,squarep)
+    #ebene:(x-p)*n=0 -> (x*n=p*n)
+    p=squarep-gaussnewton(squarep)
+    n=f_(squarep)
+    #n=n/(np.linalg.norm(n,axis=-1)**2)[:,None]#small derivative-> higher weight. TODO smaller f-> higher weight
+    n=n/f(squarep)[:,None]
+    pn=np.einsum('ij,ij->i', n, p)#dot along axis
+    #pn=pn/f(squarep)
 
-squarep=points[squareidx]
-#squarev = np.apply_along_axis(gaussnewton, -1,squarep)
-#ebene:(x-p)*n=0 -> (x*n=p*n)
-p=squarep-gaussnewton(squarep)
-n=f_(squarep)
-#n=n/(np.linalg.norm(n,axis=-1)**2)[:,None]#small derivative-> higher weight. TODO smaller f-> higher weight
-n=n/f(squarep)[:,None]
-pn=np.einsum('ij,ij->i', n, p)#dot along axis
-#pn=pn/f(squarep)
+    squareoffset=squarep[0,festeaxis]# offset along axis
+    A=np.delete(n, festeaxis, -1)
+    b=pn-squareoffset * n[:,festeaxis]
+    # we want to solve A@(x1,x2,x3).T=b but we already know for example x1
+    #(x1 would mean that festeaxis=0)
+    #we solve for x2,x3 by modifying A and b
+    #A@(x1,x2,x3)=b->(A1,A2,A3)@(x1,x2,x3).T=b
+    #-> x1*A1+(A2,A3)@(x2,x3).T=b
+    #-> (A2,A3)@(x2,x3).T=b-x1*A1
 
-squareoffset=squarep[0,festeaxis]
-A=np.delete(n, festeaxis, -1)
-b=pn-squareoffset * n[:,festeaxis]
+    x,*_=np.linalg.lstsq(A,b)
+    xfull=np.insert(x, festeaxis, squareoffset, axis=-1)
+    return xfull,sum((b-A@x)**2),x
 
-x,*_=np.linalg.lstsq(A,b)
-xfull=np.insert(x, festeaxis, squareoffset, axis=-1)
-
-print(squareoffset,x)
+#print(squareoffset,x)
 #print(p)
 #print(n)
 #print(pn)
 #print(square)
 #print(points)
 #exit()
-print(np.linalg.lstsq(A,b))
-print(sum((b-A@x)**2))
+#print(np.linalg.lstsq(A,b))
+#print(sum((b-A@x)**2))
+predictedpoints=[]
+
+for i,o in [(0,[0,0,0]),(0,[1,0,0]),(1,[0,0,0]),(1,[0,1,0]),(2,[0,0,0]),(2,[0,0,1])]:
+    xfull,e,x=evalsquare(i,o)
+    pointonside=np.all((-1<=x) & (x<=1))
+    predictedpoints.append(xfull)
+    print(xfull,e,pointonside)
 plotter = pv.Plotter()
 plotter.add_mesh(pdata)
-plotter.add_mesh(pv.PolyData([xfull]), color='red', point_size=10)
+plotter.add_mesh(pv.PolyData(predictedpoints), color='red', point_size=10)
 plotter.add_mesh(grid, color='white', opacity=0.5)  # Add grid as a background
 plotter.add_arrows(points.reshape(-1,3), -vectors.reshape(-1,3), mag=1)  # Add arrows for vector field
 plotter.show()
